f94bc074cffb01052539568d7d01c3689a96da25
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-03 14:57:23 -0400

    2021-08-03 Updated to hold previously searched cities in local storage correctly. Revised URL searches to be individual functions. Worked out bugs for displaying any previously searched city name correctly. Add 404 error page

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ let latLonCoordinates = "http://api.openweathermap.org/geo/1.0/direct?q="
 let weatherAPIKey = "f03ee938eeff29bc82f25e6ffcc8969d";
 let weatherIconURL = "http://openweathermap.org/img/wn/";
 let weatherIconClose = "@2x.png";
-let today = moment().format('MMM Do');
+let today = moment().format('ddd MMM Do');
 /**
  * 
  */
@@ -45,7 +45,8 @@ let city = [
         zipCode: ""
     }
 ];
-
+//for storing multiple objects of city
+let cities = [];
 // let storedCityWeather = [
 //     {
 //         city: [],
@@ -57,27 +58,32 @@ let city = [
 //     }
 // ]
 /**
+ * From stackoverflow.com capitalize in general for english
+ * @param {*} string 
+ * @returns 
+ */
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+/**
  * 
- * @param {*} returnedObject 
+ * @param {*} returnedObject - JSON returned from API of open weather
  *  .temp
  *  humidity
  *  wind_speed
  */
-function currentDayResult(returnedObject){
-   
-
-    document.querySelector('#currentDayHeader').textContent = city.name.toLocaleUpperCase()+" on this "+today;
+function currentDayResult(cityName, returnedObject){
+    document.querySelector('#currentDayHeader').textContent = cityName+" on this "+today;
     document.querySelector('#currentDayIcon').setAttribute("src", weatherIconURL+returnedObject.weather[0].icon+weatherIconClose);
     document.querySelector('#tempCurrentDay').textContent = returnedObject.temp+"°F";
     document.querySelector('#windCurrentDay').textContent = returnedObject.wind_speed+" MPH";
     document.querySelector('#humidityCurrentDay').textContent = returnedObject.humidity+" %";
     document.querySelector('#uvIndex').textContent = returnedObject.uvi;
-
 }
 
 function fiveDayResult(dailyObject){
     for(var i = 1; i < 6 ; i++){
-        document.querySelector('#forecastDate'+i).textContent = moment().add(i, 'd').format("dd Do");
+        document.querySelector('#forecastDate'+i).textContent = moment().add(i, 'd').format("ddd Do");
         document.querySelector('#forceastIcon'+i).setAttribute("src", weatherIconURL+dailyObject[i].weather[0].icon+weatherIconClose);
         document.querySelector('#tempForcastDay'+i).textContent = dailyObject[i].temp.max+"°F";
         document.querySelector('#windForcastDay'+i).textContent = dailyObject[i].wind_speed+"MPH";
@@ -86,62 +92,49 @@ function fiveDayResult(dailyObject){
 }
 
 /**
- * 
+ * expecting city style object. this object is based on the JSON Openweathermap.org returns
  */
-function getLatAndLonResult(foundCity){
+function storeLatAndLonResult(foundCity){
+    //clear out previous cities
+    city = {}
+    city.name = capitalizeFirstLetter(foundCity.name);
     city.lat = foundCity.lat;
     city.lon = foundCity.lon;
     city.state = foundCity.state;
     city.country = foundCity.country;
+    var passingURL = weatherURLQuery+city.lat+weaterhLonQuery+city.lon+weatherUnitsQuery[0]+weatherExclusionQuery[0]+weatherExclusionQuery[1]+","+weatherExclusionQuery[2]+","+weatherExclusionQuery[3]+weatherAPIKeyQuery+weatherAPIKey;
+    saveLocal();
+    listCity(city);
+    getWeather(city.name, passingURL);
+}
 
-    fetch(weatherURLQuery+city.lat+weaterhLonQuery+city.lon+weatherUnitsQuery[0]+weatherExclusionQuery[0]+weatherExclusionQuery[1]+","+weatherExclusionQuery[2]+","+weatherExclusionQuery[3]+weatherAPIKeyQuery+weatherAPIKey)
-        .then(function(response){
-            console.log(response);
-            if (response.status !== 200) {
-                // If the page is not on the 404 page, redirect to it.
-                document.location.replace(redirectUrl);
-            } else {
-                return response.json();
-            }
-        }).then(function (data){
-            console.log(data);
-             currentDayResult(data.current);
-             fiveDayResult(data.daily);
-             showtime();
-             listCity();
-        })
-
+function getWeather(currentCity, weatherSearchURL){
+    fetch(weatherSearchURL)
+    .then(function(response){
+        if (response.status !== 200) {
+            // redirect to 404 page if weather doesn't return
+            document.location.replace(redirectUrl);
+        } else {
+            return response.json();
+        }
+    }).then(function (data){
+         currentDayResult(currentCity, data.current);
+         fiveDayResult(data.daily);
+         showtime();
+    })
 }
 
 searchBtn.addEventListener("click", function(){
-    
     if(cityInputEl.value !== null){
-  
         var urlToFetch = "";
         var checkName = cityInputEl.value;  
         var cityState = checkName.split(",")
         if(cityState.length > 1){
-            city.name = cityState[0].trim();
-            city.state = cityState[1].trim();
-            urlToFetch = latLonCoordinates+city.name+","+city.state+",US&limit=1"+weatherAPIKeyQuery+weatherAPIKey;
+            urlToFetch = latLonCoordinates+cityState[0].trim()+","+cityState[1].trim()+",US&limit=1"+weatherAPIKeyQuery+weatherAPIKey;
         } else{
-            city.name = checkName.trim();
-            urlToFetch = latLonCoordinates+city.name+"&limit=1"+weatherAPIKeyQuery+weatherAPIKey;
+            urlToFetch = latLonCoordinates+checkName.trim()+"&limit=1"+weatherAPIKeyQuery+weatherAPIKey;
         }
-        console.log(urlToFetch);
-        fetch(urlToFetch)
-            .then(function(response){
-                console.log(response);
-                if (response.status !== 200) {
-                    // If the page is not on the 404 page, redirect to it.
-                    document.location.replace(redirectUrl);
-                } else {
-                    return response.json();
-                }
-            }).then(function (data){
-                console.log(data);
-                getLatAndLonResult(data[0]);
-            });
+            getLocation(urlToFetch);
         }
     }
 );
@@ -150,33 +143,70 @@ function showtime(){
     document.querySelector("#weatherOutput").removeAttribute("hidden");
 }
 
-function listCity(){
+function listCity(incomingObject){
  //   var count = citySearchedListEl.childElementCount;
     var citySearchedButtonEl = document.createElement('li');
     citySearchedButtonEl.classList = "city";
-    citySearchedButtonEl.textContent = city.name;
+    citySearchedButtonEl.textContent = incomingObject.name;
    // citySearchedButtonEl.setAttribute("id", "city"+count);
     citySearchedListEl.appendChild(citySearchedButtonEl);
-    saveLocal();
+    
 }
 
 
 function saveLocal(){
-    return;//TODO
+    cityInputEl.value = "";
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
 }
 
 citySearchedListEl.addEventListener("click", function(event){
     event.preventDefault();
-
     var element = event.target;
-    debugger
     if(element.matches("li")===true){
-        var requestedCity = element.innerText();
+        var requestedCity = element.innerText;
         loadPreviousCity(requestedCity);
     }
-
 })
 
-function loadPreviousCity(){
-    return;//TODO
+function getLocation(passedURL){
+    fetch(passedURL)
+        .then(function(response){
+            console.log(response);
+            if (response.status !== 200) {
+                // If the page is not on the 404 page, redirect to it.
+                debugger
+                document.location.replace(redirectUrl);
+            } else {
+                return response.json();
+            }
+        }).then(function (data){
+            storeLatAndLonResult(data[0]);
+        });
 }
+
+function reloadCities(){
+    var storedCities = JSON.parse(localStorage.getItem('cities'));
+    if(storedCities !== null){
+        cities = storedCities;
+    }
+    if(cities.length>0){
+        for(var i = 0; i < cities.length; i++){
+            listCity(cities[i]);
+        }
+    }
+}
+
+function loadPreviousCity(requestedCity){
+    debugger
+    var index = cities.findIndex(x => x.name == requestedCity);
+    //step through procedure again
+    if(index > -1){
+        var passingURL = weatherURLQuery+cities[index].lat+weaterhLonQuery+cities[index].lon+weatherUnitsQuery[0]+weatherExclusionQuery[0]+weatherExclusionQuery[1]+","+weatherExclusionQuery[2]+","+weatherExclusionQuery[3]+weatherAPIKeyQuery+weatherAPIKey;
+        getWeather(cities[index].name, passingURL);
+    }
+}
+
+
+//On page load retrieve any cities stored in local storage
+reloadCities();
